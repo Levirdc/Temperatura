3ec51d3fdb54837fd67a9238092af7afddee8957
Levi Rodrigues <levirdc@hotmail.com>
2021-08-24 21:39:03 -0300

    changed view from console to window

--- src/Temperatura/Temperatura.java
+++ src/Temperatura/Temperatura.java
@@ -3,6 +3,8 @@ package Temperatura;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Temperatura {
 
 	public static void main(String[] args) {
@@ -10,22 +12,23 @@ public class Temperatura {
 		double c, f;
 		
 		//objetos
-		Scanner teclado = new Scanner(System.in);
+		//Scanner teclado = new Scanner(System.in);
 		DecimalFormat formatador = new DecimalFormat("#0.00");
 		
 		//entrada
 		System.out.println("Conversão de Temperatura");
-		System.out.print("Digite a Temperatura em Fahrenheit: ");
+		//System.out.print("Digite a Temperatura em Fahrenheit: ");
 		
-		f= teclado.nextDouble();
-		
+		//f= teclado.nextDouble();
+		f = Double.parseDouble(JOptionPane.showInputDialog("Insira a Temperatura em Fahreinheit"));
 		//processameto
 		
 		c = (5*(f - 32)) /9;
 		
 		//saída
-		System.out.println("Temperatura em Celsius: " + formatador.format(c) + " °C");
-		teclado.close();
+		JOptionPane.showMessageDialog(null, formatador.format(c));
+		//System.out.println("Temperatura em Celsius: " + formatador.format(c) + " °C");
+		//teclado.close();
 	}
 
 }
